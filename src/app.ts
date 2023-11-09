b1d37e0e2c72861b3af7249a7f84ad75f46c3882
André Viapiana <andreviapiana@gmail.com>
2023-11-09 16:55:31 -0300

    ✨feat: Casos de Uso - Criação de Organização

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,50 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from '@/lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+// Rota de Criação de Organizações //
+app.post('/register', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+    city: z.string(),
+    address: z.string(),
+    postalCode: z.string(),
+    whatsAppNumber: z.string(),
+  })
+
+  const { name, email, password, city, address, postalCode, whatsAppNumber } =
+    registerBodySchema.parse(request.body)
+
+  await prisma.organization.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+      city,
+      address,
+      postalCode,
+      whatsAppNumber,
+    },
+  })
+
+  return reply.status(201).send()
+})
+
+/* const prisma = new PrismaClient()
 
 prisma.organization.create({
   data: {
     id: '30ab4c94-593c-4a5b-8249-54364ef77612',
     postalCode: '01310‑900',
+    city: 'São Paulo',
     address: 'Avenida Paulista, 52',
     email: 'adote_pets@email.com',
     name: 'Adote Pets',
-    password: '123456',
+    password_hash: '123456',
     whatsAppNumber: '+558699999999',
   },
 })
@@ -26,3 +58,4 @@ prisma.pet.create({
     size: 'MEDIO',
   },
 })
+ */
