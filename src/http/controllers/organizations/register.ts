273fbefe6f7033a941251a40d331f009203cf6de
André Viapiana <andreviapiana@gmail.com>
2023-11-09 17:14:21 -0300

    ✨feat: Casos de Uso - Hash da Senha e Validação - Bcrypt

--- src/http/controllers/organizations/register.ts
+++ src/http/controllers/organizations/register.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+import { hash } from 'bcryptjs'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -16,11 +17,25 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password, city, address, postalCode, whatsAppNumber } =
     registerBodySchema.parse(request.body)
 
+  const password_hash = await hash(password, 6)
+
+  const userWithSameInfos = await prisma.organization.findUnique({
+    where: {
+      email,
+      name,
+      whatsAppNumber,
+    },
+  })
+
+  if (userWithSameInfos) {
+    return reply.status(409).send()
+  }
+
   await prisma.organization.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
       city,
       address,
       postalCode,
