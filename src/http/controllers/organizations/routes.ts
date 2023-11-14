c25c174605d9cb1dd6be29cd0f7d3bb469808eb9
André Viapiana <andreviapiana@gmail.com>
2023-11-14 09:47:06 -0300

    ✨feat: Casos de Uso - Controller do Caso de Uso de Perfil

--- src/http/controllers/organizations/routes.ts
+++ src/http/controllers/organizations/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './register'
 import { authenticate } from './authenticate'
+import { profile } from './profile'
 
 export async function organizationRoutes(app: FastifyInstance) {
   app.post('/register', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/profile', profile)
 }
