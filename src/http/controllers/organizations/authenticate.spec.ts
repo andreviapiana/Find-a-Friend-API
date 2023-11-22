1d630ba7266838dbd0903160e47ba0c5196fc583
André Viapiana <andreviapiana@gmail.com>
2023-11-22 17:05:37 -0300

    ✨feat: Testes E2E - Teste E2E do Registro e de Autenticação - Supertest

--- src/http/controllers/organizations/authenticate.spec.ts
+++ src/http/controllers/organizations/authenticate.spec.ts
@@ -1,3 +1,35 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/register').send({
+      name: 'Adote Pets',
+      email: 'adote_pets@email.com',
+      password: '123456',
+      city: 'São Paulo',
+      address: 'Avenida Paulista, 52',
+      postalCode: '01310‑900',
+      whatsAppNumber: '+558699999999',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'adote_pets@email.com',
+      password: '123456',
+    })
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
--- src/http/controllers/organizations/register.spec.ts
+++ src/http/controllers/organizations/register.spec.ts
@@ -1,3 +1,27 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/register').send({
+      name: 'Adote Pets',
+      email: 'adote_pets@email.com',
+      password: '123456',
+      city: 'São Paulo',
+      address: 'Avenida Paulista, 52',
+      postalCode: '01310‑900',
+      whatsAppNumber: '+558699999999',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
